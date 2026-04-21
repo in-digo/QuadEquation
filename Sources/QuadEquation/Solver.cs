12772f1815ff27c517c31d8850c48e6c37932430
John Doe <burov.yarv@gmail.com>
2026-04-21 22:38:31 +0300

    6. Минимальная реализация функции solve, которая удовлетворяет тесту из п.5

--- Sources/QuadEquation/Solver.cs
+++ Sources/QuadEquation/Solver.cs
@@ -4,6 +4,14 @@ public class Solver
 {
     public double[] Solve(double a, double b, double c)
     {
-        return [];
+        var discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0)
+            return [];
+
+        var x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+        var x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+        return [x1, x2];
     }
 }
